2491dee01dfdc5e1f0a4c764d20b744f3326bff8
Juan Monetta <jpmonettas@gmail.com>
2018-04-02 10:36:51 -0300

    Moved tree layout to library

--- src/cljs_flowgraph/core.cljs
+++ src/cljs_flowgraph/core.cljs
@@ -1,9 +1,10 @@
 (ns cljs-flowgraph.core
   (:require [reagent.core :as r]
+            [clj-tree-layout.core :refer [layout-tree]]
             [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]))
 
-(defonce comp-hiccup
-  {'let (fn [d]
+(defonce comp-hiccup {}
+  #_{'let (fn [d]
           [:div (str (:id d) " Let " (:width d) "x" (:height d))])
    'map (fn [d]
           [:div {:style {:background-color "#c7d493"}}
@@ -18,75 +19,8 @@
           [:li (str "Height: " (:height d))]]])})
 
 
-(def *hgap* 50)
-(def *vgap* 50)
-
-(defn annotate-tree [tree]
-  (let [x-numbers (atom {})
-        aux (fn aux [t depth]
-              (-> t
-                  (assoc :depth depth
-                         :depth-order (-> (swap! x-numbers update depth (fnil inc 0))
-                                (get depth)))
-                  (update :childs (fn [chlds]
-                                    (mapv (fn [c]
-                                            (aux c (inc depth)))
-                                          chlds)))))]
-    (aux tree 0)))
-
-(defn tree-contours [t]
-  (->> (tree-seq :childs :childs t)
-       (group-by :depth)
-       (map (fn [[l nodes]]
-              (let [fnode (apply (partial min-key :depth-order) nodes)
-                    lnode (apply (partial max-key :depth-order) nodes)]
-               [l (:x fnode) (+ (:x lnode) (:width lnode))])))
-       (sort-by first)
-       (reduce (fn [r [_ left right]]
-                 (-> r
-                     (update :left conj left)
-                     (update :right conj right)))
-               {:left [] :right []})))
-
-
-(defn max-distance [ns1 ns2]
-  (apply max (map #(Math/abs (- %1 %2)) ns1 ns2)))
-
-(defn push-tree [t delta]
-  (-> t
-      (update :x #(+ % delta))
-      (update :childs (fn [chlds] (mapv #(push-tree % delta) chlds)))))
-
-
-(defn layout-tree
-  ([node] (layout-tree node 0))
-  ([{:keys [width height depth childs id] :as node} y]
-   (if (not-empty childs)
-     (let [layout-childs (mapv #(layout-tree % (+ y height *vgap*)) childs)
-           pushed-childs (loop [pusheds [(first layout-childs)]
-                                [c & r] (rest layout-childs)]
-                           (if c
-                             (let [right-contour (:right (tree-contours (last pusheds)))
-                                   left-contour (:left (tree-contours c))
-                                   delta (+ (max-distance right-contour left-contour) *hgap*)]
-                               (recur (conj pusheds (push-tree c delta)) r))
-                             pusheds))
-           firstc (first pushed-childs)
-           lastc (last pushed-childs)
-           childs-width (- (+ (:x lastc) (:width lastc)) (:x firstc))]
-       (assoc node
-              :x (+ (/ childs-width 2) (:x firstc))
-              :childs pushed-childs
-              :y y))
-     (assoc node :x 0 :y y))))
-
-(defn layout [t]
-  (-> t
-      annotate-tree
-      layout-tree))
-
 (defn assoc-sizes [n sizes-map]
-  (let [[width height] (get sizes-map (:id n))]
+  (let [[width height] (get sizes-map (-> n :data :id))]
    (assoc n
           :width width
           :height height
@@ -100,19 +34,23 @@
                             (assoc r (.-id n) [(.-offsetWidth n) (.-offsetHeight n)]))
                           {}
                           (array-seq (.-childNodes dn)))
-        nodes-with-sizes (layout (assoc-sizes @nodes-a dom-sizes))]
+        nodes-with-sizes (layout-tree (assoc-sizes @nodes-a dom-sizes)
+                                      {:data-fn :data
+                                       :children-fn :childs
+                                       :width-fn :width
+                                       :height-fn :height})]
     (.log js/console "Nodes recalculated " nodes-with-sizes)
     (reset! nodes-a nodes-with-sizes)))
 
-(defn node [{:keys [id x y width height type] :as n}]
+(defn node [{:keys [data x y width height] :as n}]
   [:div
-   {:id id
+   {:id (:id data)
     :style {:position :absolute
             :top y :left x
             :border "1px solid black"
             :padding "10px"
             :border-radius "10px"}}
-   ((get comp-hiccup type) n)])
+   ((get comp-hiccup (:type data) (constantly [:div (:id data)])) data)])
 
 (defn flowgraph [nodes width height]
   (let [nodes-a (r/atom nodes)]
@@ -124,7 +62,7 @@
                        (let [nodes-seq (tree-seq :childs :childs @nodes-a)
                              links (for [n nodes-seq
                                          cn (:childs n)]
-                                     [(str (:id n) (:id cn))
+                                     [(str (-> n :data :id) (-> cn :data :id))
                                       (+ (:x n) (/ (:width n) 2))
                                       (+ (:y n) (:height n))
                                       (+ (:x cn) (/ (:width cn) 2)) 
@@ -132,30 +70,33 @@
                         [:div#panel {:style {:position :absolute
                                              :top 150
                                              :width width
-                                             :height height
-                                             :background-color "#eee"}}
+                                             :height height}}
+
+                         ;; Links
                          [:svg {:width width
                                 :height height}
                           (for [[lid x1 y1 x2 y2] links]
-                            ^{:key lid}
+                            ^{:key (str "L" lid)}
                             [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2
                                     :style {:stroke :red
                                             :stroke-width 2}}])]
+
+                         ;; Nodes
                          (for [n nodes-seq]
-                           ^{:key (:id n)} [node n])]))})))
+                           ^{:key (->> n :data :id (str "N"))} [node n])]))})))
  
-(defonce app-state (r/atom {:id "1" :type 'let
-                            :childs [{:id "11" :type 'map 
-                                      :childs [{:id "111" :type 'x}
-                                               {:id "112" :type 'x}
-                                               {:id "113" :type 'x}
-                                               {:id "114" :type 'x}]}
-                                     {:id "12" :type '+
-                                      :childs [{:id "121" :type 'x 
-                                                :childs [{:id "1211" :type 'x }]}
-                                               {:id "122" :type 'x :width 26 :height 20}
-                                               {:id "123" :type 'x :width 26 :height 20}
-                                               {:id "124" :type 'x :width 26 :height 20}]}]}))
+(defonce app-state (r/atom {:data {:id "1" :type 'let}
+                            :childs [{:data {:id "11" :type 'map} 
+                                      :childs [{:data {:id "111" :type 'x}}
+                                               {:data {:id "112" :type 'x}}
+                                               {:data {:id "113" :type 'x}}
+                                               {:data {:id "114" :type 'x}}]}
+                                     {:data {:id "12" :type '+}
+                                      :childs [{:data {:id "121" :type 'x} 
+                                                :childs [{:data {:id "1211" :type 'x} }]}
+                                               {:data {:id "122" :type 'x}}
+                                               {:data {:id "123" :type 'x}}
+                                               {:data {:id "124" :type 'x}}]}]}))
 
 
 
