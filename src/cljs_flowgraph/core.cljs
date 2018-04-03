b1a00b678252137e9dce0a7df814ce4ee73e2cbf
Juan Monetta <jpmonettas@gmail.com>
2018-04-03 14:41:38 -0300

    Now it uses latest clj-tree-layout api

--- src/cljs_flowgraph/core.cljs
+++ src/cljs_flowgraph/core.cljs
@@ -1,32 +1,10 @@
 (ns cljs-flowgraph.core
   (:require [reagent.core :as r]
-            [clj-tree-layout.core :refer [layout-tree]]
+            [clj-tree-layout.core :refer [layout-tree] :as tl]
             [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]))
 
-(defonce comp-hiccup {}
-  #_{'let (fn [d]
-          [:div (str (:id d) " Let " (:width d) "x" (:height d))])
-   'map (fn [d]
-          [:div {:style {:background-color "#c7d493"}}
-           (str (:id d) " Map "(:width d) "x" (:height d))])
-   '+ (fn [d]
-        [:div (str (:id d) " + "(:width d) "x" (:height d))])
-   'x (fn [d]
-        [:div
-         [:b (:id d)] [:span "X"]
-         [:ul
-          [:li (str "Width: " (:width d))]
-          [:li (str "Height: " (:height d))]]])})
 
-
-(defn assoc-sizes [n sizes-map]
-  (let [[width height] (get sizes-map (-> n :data :id))]
-   (assoc n
-          :width width
-          :height height
-          :childs (mapv #(assoc-sizes % sizes-map) (:childs n)))))
-
-(defn calculate-and-redraw [panel-node-comp nodes-a]
+(defn dimension-and-redraw [panel-node-comp internal-nodes dimensions-map-a childs-fn branch-fn]
   (let [dn (r/dom-node panel-node-comp)
         dom-width (.-offsetWidth dn)
         dom-height (.-offsetHeight dn)
@@ -34,80 +12,94 @@
                             (assoc r (.-id n) [(.-offsetWidth n) (.-offsetHeight n)]))
                           {}
                           (array-seq (.-childNodes dn)))
-        nodes-with-sizes (layout-tree (assoc-sizes @nodes-a dom-sizes)
-                                      {:data-fn :data
-                                       :children-fn :childs
-                                       :width-fn :width
-                                       :height-fn :height})]
-    (.log js/console "Nodes recalculated " nodes-with-sizes)
-    (reset! nodes-a nodes-with-sizes)))
+        dimensions (layout-tree internal-nodes
+                                {:sizes dom-sizes
+                                 :childs-fn :childs
+                                 :id-fn :node-id
+                                 :branch-fn :childs
+                                 :h-gap 5
+                                 :v-gap 50})]
+    (.log js/console "Dimensions recalculated " dimensions)
+    (reset! dimensions-map-a dimensions)))
 
-(defn node [{:keys [data x y width height] :as n}]
+(defn node [id n x y render-fn]
   [:div
-   {:id (:id data)
+   {:id id
     :style {:position :absolute
-            :top y :left x
-            :border "1px solid black"
-            :padding "10px"
-            :border-radius "10px"}}
-   ((get comp-hiccup (:type data) (constantly [:div (:id data)])) data)])
+            :top y :left x}}
+   (render-fn n)])
 
-(defn flowgraph [nodes width height]
-  (let [nodes-a (r/atom nodes)]
+(defn build-internal-tree [t branch-fn childs-fn]
+  {:node-id (str (random-uuid))
+   :original-node t
+   :childs (mapv #(build-internal-tree % branch-fn childs-fn)
+                 (childs-fn t))})
+
+(defn flowgraph [nodes & {:keys [layout-width layout-height branch-fn childs-fn render-fn]}]
+  (let [internal-nodes (build-internal-tree nodes branch-fn childs-fn)
+        dimensions-map-a (r/atom {})]
    (r/create-class
-    {:component-did-mount (fn [this] (calculate-and-redraw this nodes-a))
+    {:component-did-mount (fn [this] (dimension-and-redraw this internal-nodes dimensions-map-a childs-fn branch-fn))
      ;; :component-did-update (fn [this] (calculate-and-redraw this nodes-a))
-     :component-will-receive-props (fn [this [_ new-nodes]] (reset! nodes-a new-nodes))
+     ;; :component-will-receive-props (fn [this [_ new-nodes]] (reset! nodes-a new-nodes))
      :reagent-render (fn [_]
-                       (let [nodes-seq (tree-seq :childs :childs @nodes-a)
-                             links (for [n nodes-seq
-                                         cn (:childs n)]
-                                     [(str (-> n :data :id) (-> cn :data :id))
-                                      (+ (:x n) (/ (:width n) 2))
-                                      (+ (:y n) (:height n))
-                                      (+ (:x cn) (/ (:width cn) 2)) 
-                                      (:y cn)])]
+                       (let [nodes-seq (tree-seq :childs :childs internal-nodes)
+                             dimensions @dimensions-map-a
+                             links (when (not-empty dimensions)
+                                     (clog (for [n nodes-seq
+                                            cn (:childs n)]
+                                        (let [[x y width height] (get dimensions (:node-id n))
+                                              [cx cy cwidth cheight] (get dimensions (:node-id cn))]
+                                          [(str (:node-id n) (:node-id cn))
+                                           (+ x (/ width 2))
+                                           (+ y height)
+                                           (+ cx (/ cwidth 2)) 
+                                           cy]))
+                                           :js))]
                         [:div#panel {:style {:position :absolute
                                              :top 150
-                                             :width width
-                                             :height height}}
+                                             :width layout-width
+                                             :height layout-height}}
 
                          ;; Links
-                         [:svg {:width width
-                                :height height}
+                         [:svg {:width layout-width
+                                :height layout-height}
                           (for [[lid x1 y1 x2 y2] links]
-                            ^{:key (str "L" lid)}
+                            ^{:key lid}
                             [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2
                                     :style {:stroke :red
                                             :stroke-width 2}}])]
 
                          ;; Nodes
                          (for [n nodes-seq]
-                           ^{:key (->> n :data :id (str "N"))} [node n])]))})))
- 
-(defonce app-state (r/atom {:data {:id "1" :type 'let}
-                            :childs [{:data {:id "11" :type 'map} 
-                                      :childs [{:data {:id "111" :type 'x}}
-                                               {:data {:id "112" :type 'x}}
-                                               {:data {:id "113" :type 'x}}
-                                               {:data {:id "114" :type 'x}}]}
-                                     {:data {:id "12" :type '+}
-                                      :childs [{:data {:id "121" :type 'x} 
-                                                :childs [{:data {:id "1211" :type 'x} }]}
-                                               {:data {:id "122" :type 'x}}
-                                               {:data {:id "123" :type 'x}}
-                                               {:data {:id "124" :type 'x}}]}]}))
+                           (let [[x y _ _] (get dimensions (:node-id n))]
+                             ^{:key (:node-id n)} [node (:node-id n) (:original-node n) x y render-fn]))]))})))
 
+(defonce app-state (r/atom '(+ 1 2 (- 4 2) (/ 123 3) (inc 25))))
 
+(defmulti render-node symbol?)
+
+(defmethod render-node true [n]
+  [:div {:style {:background-color :yellow
+                 :border "1px solid black"
+                 :padding "10px"
+                 :border-radius "10px"}} (str n)])
+
+(defmethod render-node :default [n]
+  [:div {:style {:border "1px solid black"
+                 :padding "10px"
+                 :border-radius "10px"}}
+   (str n)])
 
 (defn app []
   [:div
-   [flowgraph @app-state 1500 500]])
+   [flowgraph @app-state
+    :layout-width 1500 
+    :layout-height 500
+    :branch-fn #(when (seq? %) %)
+    :childs-fn #(when (seq? %) %)
+    :render-fn render-node]])
 
 (defn init []
   (r/render [app]
-            (.getElementById js/document "app"))
-  (.log js/console "Done!!!!!")) 
-
-
-#_(swap! app-state assoc-in ["1" :label] "baaaaaaa")
+            (.getElementById js/document "app"))) 
